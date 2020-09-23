94cab1833b18a9d2ebf020b015c92e08e6f70b35
pablo <358126+marce1994@users.noreply.github.com>
2020-09-22 23:16:55 -0300

    first commit

---
+++ game/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : Singleton
+{
+    private Dictionary<string, Queue<GameObject>> _pool;
+
+    public class PooleableSources
+    {
+        public string resourcePath;
+        public GameObject[] prefabs;
+    }
+
+    public GameObject RecicleGameObject(string key, GameObject gameObject)
+    {
+        EnsureInitialized(key);
+
+        gameObject.SetActive(false);
+        _pool[key].Enqueue(gameObject);
+
+        return gameObject;
+    }
+
+    public GameObject InstantiateFromPool(string key, Vector3 position, Quaternion rotation)
+    {
+        EnsureInitialized(key);
+
+        var obj = _pool[key].Dequeue();
+        
+        obj.transform.position = position;
+        obj.transform.rotation = rotation;
+
+        obj.SetActive(true);
+
+        return _pool[key].Dequeue();
+    }
+
+    private void EnsureInitialized(string key)
+    {
+        if (_pool == null)
+            _pool = new Dictionary<string, Queue<GameObject>>();
+        if (!_pool.ContainsKey(key))
+            _pool.Add(key, new Queue<GameObject>());
+    }
+}
---
+++ game/Assets/Scripts/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    public int number;
+    private static Singleton instance;
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new GameObject("Singleton").AddComponent<Singleton>();
+            return instance;
+        }
+    }
+}
